c70ac97f31fb555bb87bbed41a3b5b85ab459e7d
Aaron Huisinga <aaron@platform.marketing>
2016-11-16 11:35:10 -0600

    Bump to v1.2.2
    1. Fix width of testimonials when only 1 or 2 are given.

--- templates/first-responders-page.php
+++ templates/first-responders-page.php
@@ -224,7 +224,7 @@ if ($hover_setting && $hover_setting != '') {
                                 <span style="color: rgb(34, 34, 34); font-family: arial, sans-serif; font-size: 13px; letter-spacing: normal; line-height: normal; text-align: start;"><?= $test_1_job ?></span>
                             </p>
                             <p class="role-element leadstyle-text">
-                                <i style="color: rgb(47, 47, 47); font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 16px; letter-spacing: normal; line-height: 23px; text-align: start;">"<?= $test_1_text ?>"</i><br>
+                                <i style="color: rgb(47, 47, 47); font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 16px; letter-spacing: normal; line-height: 23px; text-align: start;"><?= $test_1_text ?></i><br>
                             </p>
                         </div>
                     </div>
@@ -247,7 +247,7 @@ if ($hover_setting && $hover_setting != '') {
                                 <span style="color: rgb(34, 34, 34); font-family: arial, sans-serif; font-size: 13px; letter-spacing: normal; line-height: normal; text-align: start;"><?= $test_2_job ?></span>
                             </p>
                             <p class="role-element leadstyle-text">
-                                <i style="color: rgb(47, 47, 47); font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 16px; letter-spacing: normal; line-height: 23px; text-align: start;">"<?= $test_2_text ?>"</i>
+                                <i style="color: rgb(47, 47, 47); font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 16px; letter-spacing: normal; line-height: 23px; text-align: start;"><?= $test_2_text ?></i>
                             </p>
                         </div>
                     </div>
@@ -270,7 +270,7 @@ if ($hover_setting && $hover_setting != '') {
                                 <span style="color: rgb(34, 34, 34); font-family: arial, sans-serif; font-size: 13px; letter-spacing: normal; line-height: normal; text-align: start;"><?= $test_3_job ?></span>
                             </p>
                             <p class="role-element leadstyle-text">
-                                <i style="color: rgb(47, 47, 47); font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 16px; letter-spacing: normal; line-height: 23px; text-align: start;">"<?= $test_3_text ?>"</i>
+                                <i style="color: rgb(47, 47, 47); font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 16px; letter-spacing: normal; line-height: 23px; text-align: start;"><?= $test_3_text ?></i>
                             </p>
                         </div>
                     </div>
