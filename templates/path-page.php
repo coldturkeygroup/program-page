deaecbafab48736ad0f91fb73c747bb7a56ba91a
Aaron Huisinga <aaron@platform.marketing>
2016-12-06 18:20:21 -0600

    Bump to version 1.4.1
    1. Fix URLs for PATH Program favicons

--- templates/path-page.php
+++ templates/path-page.php
@@ -77,11 +77,11 @@ if ($hover_setting && $hover_setting != '') {
     <title><?= $title ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
     <?php wp_head(); ?>
-    <link rel="apple-touch-icon" sizes="180x180" href="https://cdn.platform.marketing/assets/programs/path/apple-touch-icon.png">
-    <link rel="icon" type="image/png" href="https://cdn.platform.marketing/assets/programs/path/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="https://cdn.platform.marketing/assets/programs/path/favicon-16x16.png" sizes="16x16">
-    <link rel="manifest" href="https://cdn.platform.marketing/assets/programs/path/manifest.json">
-    <link rel="mask-icon" href="https://cdn.platform.marketing/assets/programs/path/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="apple-touch-icon" sizes="180x180" href="https://cdn.platform.marketing/assets/programs/path/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="https://cdn.platform.marketing/assets/programs/path/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="https://cdn.platform.marketing/assets/programs/path/favicons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="https://cdn.platform.marketing/assets/programs/path/favicons/manifest.json">
+    <link rel="mask-icon" href="https://cdn.platform.marketing/assets/programs/path/favicons/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="theme-color" content="#ffffff">
     <style>
         <?php
