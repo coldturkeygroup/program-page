8cd277aaef338a03740c0dcdb71d492000e33b25
Aaron Huisinga <aaron@platform.marketing>
2017-08-29 12:56:15 -0500

    Bump to v1.6.1
    1. Update Composer dependencies

--- program-page.php
+++ program-page.php
@@ -4,7 +4,7 @@ namespace ColdTurkey\ProgramPage;
 
 /*
  * Plugin Name: Program Page
- * Version: 1.6.0
+ * Version: 1.6.1
  * Plugin URI: http://www.coldturkeygroup.com/
  * Description: Custom info pages for Platform ad campaigns
  * Author: Cold Turkey Group
@@ -17,20 +17,24 @@ namespace ColdTurkey\ProgramPage;
  * @since 1.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) {
+    exit;
+}
 
-if ( ! defined( 'PROGRAM_PAGE_PLUGIN_PATH' ) )
-	define( 'PROGRAM_PAGE_PLUGIN_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+if (!defined('PROGRAM_PAGE_PLUGIN_PATH')) {
+    define('PROGRAM_PAGE_PLUGIN_PATH', trailingslashit(plugin_dir_path(__FILE__)));
+}
 
-if ( ! defined( 'PROGRAM_PAGE_PLUGIN_VERSION' ) )
-	define( 'PROGRAM_PAGE_PLUGIN_VERSION', '1.6.0' );
+if (!defined('PROGRAM_PAGE_PLUGIN_VERSION')) {
+    define('PROGRAM_PAGE_PLUGIN_VERSION', '1.6.1');
+}
 
-require_once( 'classes/class-program-page.php' );
+require_once('classes/class-program-page.php');
 
 global $program_page;
-$program_page = new ProgramPage( __FILE__, new PlatformCRM() );
+$program_page = new ProgramPage(__FILE__, new PlatformCRM());
 
-if ( is_admin() ) {
-	require_once( 'classes/class-program-page-admin.php' );
-	new ProgramPage_Admin( __FILE__ );
+if (is_admin()) {
+    require_once('classes/class-program-page-admin.php');
+    new ProgramPage_Admin(__FILE__);
 }
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -23,6 +23,10 @@ class ComposerStaticInit6771822ed90090975b5cdbf140766183
             'GuzzleHttp\\Promise\\' => 19,
             'GuzzleHttp\\' => 11,
         ),
+        'C' => 
+        array (
+            'Composer\\Installers\\' => 20,
+        ),
     );
 
     public static $prefixDirsPsr4 = array (
@@ -42,15 +46,9 @@ class ComposerStaticInit6771822ed90090975b5cdbf140766183
         array (
             0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
         ),
-    );
-
-    public static $prefixesPsr0 = array (
-        'C' => 
+        'Composer\\Installers\\' => 
         array (
-            'Composer\\Installers\\' => 
-            array (
-                0 => __DIR__ . '/..' . '/composer/installers/src',
-            ),
+            0 => __DIR__ . '/..' . '/composer/installers/src/Composer/Installers',
         ),
     );
 
@@ -65,7 +63,6 @@ class ComposerStaticInit6771822ed90090975b5cdbf140766183
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit6771822ed90090975b5cdbf140766183::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit6771822ed90090975b5cdbf140766183::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit6771822ed90090975b5cdbf140766183::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit6771822ed90090975b5cdbf140766183::$classMap;
 
         }, null, ClassLoader::class);
